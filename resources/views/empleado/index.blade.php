5aa5570e59bc98680601c8bfcc5cd7ca0086d8e0
unknown <65382224+LuisFernandoReyes@users.noreply.github.com>
2024-07-23 23:19:01 -0600

    Making index.blade

--- resources/views/empleado/index.blade.php
+++ resources/views/empleado/index.blade.php
@@ -1,1 +1,31 @@
 Mostrar la lista de empleados
+
+<div class="table-responsive">
+    <table class="table table-primary">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Fotografia</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Apellido paterno</th>
+                <th scope="col">Apellido Materno</th>
+                <th scope="col">Correo</th>
+                <th scope="col">Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($empleados as $empleado)
+            <tr>
+                <td> {{ $empleado->id }} </td>
+                <td> {{ $empleado->foto }} </td>
+                <td> {{ $empleado->Nombre }} </td>
+                <td> {{ $empleado->ApellidoPaterno }} </td>
+                <td> {{ $empleado->ApellidoPaterno }} </td>
+                <td> {{ $empleado->correo }} </td>
+                <td> Editar | Borrar</td>
+            </tr>
+            @endforeach
+
+        </tbody>
+    </table>
+</div>

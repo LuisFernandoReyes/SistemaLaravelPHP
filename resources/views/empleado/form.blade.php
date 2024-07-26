d38fc89ab4ef7031e9abb88f4e502162819f6c95
unknown <65382224+LuisFernandoReyes@users.noreply.github.com>
2024-07-25 21:56:28 -0600

    Puttind method isset in the form just to can use de page create without put data on it

--- resources/views/empleado/form.blade.php
+++ resources/views/empleado/form.blade.php
@@ -1,21 +1,24 @@
 Formulario que tendrá los datos en común con create y edit
 
-<label for="Nombre">Nombre</label>
-    <input type="text" name="Nombre" value="{{ $empleado->Nombre }}" id="Nombre">
+    <label for="Nombre">Nombre</label>
+    <input type="text" name="Nombre" value="{{ isset($empleado->Nombre)?$empleado->Nombre:'' }}" id="Nombre">
     <br>
     <label for="ApellidoPaterno">ApellidoPaterno</label>
-    <input type="text" name="ApellidoPaterno" value="{{ $empleado->ApellidoPaterno}}" id="ApellidoPaterno">
+    <input type="text" name="ApellidoPaterno" value="{{ isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno:''}}" id="ApellidoPaterno">
     <br>
     <label for="ApellidoMaterno">ApellidoMaterno</label>
-    <input type="text" name="ApellidoMaterno" value="{{ $empleado->ApellidoMaterno}}" id="ApellidoMaterno">
+    <input type="text" name="ApellidoMaterno" value="{{ isset($empleado->ApellidoMaterno)?$empleado->ApellidoMaterno:''}}" id="ApellidoMaterno">
     <br> 
     <label for="Correo">Correo</label>
-    <input type="text" name="Correo" value="{{ $empleado->Correo }}" id="Correo">
+    <input type="text" name="Correo" value="{{ isset($empleado->Correo)?$empleado->Correo:'' }}" id="Correo">
     <br>
 
     <label for="Foto">Foto</label>
-    <img src="{{ asset('storage').'/'.$empleado->foto }}" width="100" alt="">
+    @if(isset($empleado->foto))
 
+    <img src="{{ asset('storage').'/'.$empleado->foto }}" width="100" alt="">
+    @endif
+    
     <input type="file" name="Foto" value="" id="Foto">
     <br>
 

95929862f959f2a77097798e81ecb5c3f587bbba
unknown <65382224+LuisFernandoReyes@users.noreply.github.com>
2024-07-25 21:29:54 -0600

    Making the update form

--- resources/views/empleado/edit.blade.php
+++ resources/views/empleado/edit.blade.php
@@ -1,3 +1,11 @@
 Formulario para editar empleados
 <br>
+
+<form action=" {{ url('/empleado/'.$empleado->id)}} " method="post" enctype="multipart/form-data">
+
+@csrf
+{{ method_field('PATCH') }}
 @include('empleado.form')
+
+</form>
+
